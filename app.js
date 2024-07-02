7424e1b7c7be17ab3fbee12fed5856795cc52f23
Msmoinsk <moinsk5365@gmail.com>
2024-07-02 21:48:36 +0530

    Intermidiate Backend Development Stage

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use("/api/v1/tasks", tasks)
 app.use(notFound)
 app.use(errorHandlerMiddleware)
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 const start = async() => {
     try {
@@ -32,3 +32,5 @@ const start = async() => {
     }
 }
 start()
+
+// In ternimal write this command{ PORT=6000 node app.js }
